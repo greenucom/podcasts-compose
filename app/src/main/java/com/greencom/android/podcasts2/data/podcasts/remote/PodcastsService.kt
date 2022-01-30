c4f3fff11d63515c52d2686eb3afeaa8170fcd75
Artem Sidorov <s.artem.ekb@gmail.com>
2022-01-30 14:57:23 +0300

    Add podcasts dtos

---
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcasts/remote/PodcastsService.kt
@@ -0,0 +1,5 @@
+package com.greencom.android.podcasts2.data.podcasts.remote
+
+interface PodcastsService {
+
+}
---
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcasts/remote/dto/PodcastDto.kt
@@ -0,0 +1,44 @@
+package com.greencom.android.podcasts2.data.podcasts.remote.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class PodcastDto(
+    
+    @SerialName("id")
+    val id: Long? = null,
+    
+    @SerialName("title")
+    val title: String? = null,
+    
+    @SerialName("url")
+    val url: String? = null,
+
+    @SerialName("link")
+    val link: String? = null,
+
+    @SerialName("description")
+    val description: String? = null,
+
+    @SerialName("author")
+    val author: String? = null,
+
+    @SerialName("ownerName")
+    val ownerName: String? = null,
+    
+    @SerialName("image")
+    val image: String? = null,
+    
+    @SerialName("artwork")
+    val artwork: String? = null,
+    
+    @SerialName("lastUpdateTime")
+    val lastUpdateTime: Long? = null,
+    
+    @SerialName("language")
+    val language: String? = null,
+
+    // TODO: Add "categories"
+
+)
