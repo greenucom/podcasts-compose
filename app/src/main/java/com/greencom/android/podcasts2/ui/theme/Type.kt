ea9e0978e8c0afe3809e3c74a459f08bb0811741
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-01 01:28:23 +0300

    Add Inter font

--- app/src/main/java/com/greencom/android/podcasts2/ui/theme/Type.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/theme/Type.kt
@@ -2,27 +2,88 @@ package com.greencom.android.podcasts2.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.greencom.android.podcasts2.R
 
-// Set of Material typography styles to start with
+val Inter = FontFamily(
+    Font(R.font.inter_thin, FontWeight.W100),
+    Font(R.font.inter_extra_light, FontWeight.W200),
+    Font(R.font.inter_light, FontWeight.W300),
+    Font(R.font.inter_regular, FontWeight.W400),
+    Font(R.font.inter_medium, FontWeight.W500),
+    Font(R.font.inter_semi_bold, FontWeight.W600),
+    Font(R.font.inter_bold, FontWeight.W700),
+    Font(R.font.inter_extra_bold, FontWeight.W800),
+    Font(R.font.inter_black, FontWeight.W900),
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W300,
+        fontSize = 93.sp,
+    ),
+    h2 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W300,
+        fontSize = 58.sp,
+    ),
+    h3 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 47.sp,
+    ),
+    h4 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 33.sp,
+    ),
+    h5 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 23.sp,
+    ),
+    h6 = TextStyle(
+        fontFamily = Inter,
         fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = 19.sp,
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+    ),
+    subtitle2 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+    ),
+    body1 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+    ),
+    body2 = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+    ),
+    button = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+    ),
+    overline = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.W400,
+        fontSize = 10.sp,
+    ),
 )
