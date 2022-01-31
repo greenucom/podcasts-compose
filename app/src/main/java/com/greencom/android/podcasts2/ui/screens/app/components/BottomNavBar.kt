b73864ebcc373393d0d241ed07fbbc1fec3d7597
Artem Sidorov <s.artem.ekb@gmail.com>
2022-01-31 22:39:09 +0300

    Update BottomNavBar.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/app/components/BottomNavBar.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/app/components/BottomNavBar.kt
@@ -3,11 +3,11 @@ package com.greencom.android.podcasts2.ui.screens.app.components
 import android.content.res.Configuration
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -20,31 +20,32 @@ fun BottomNavBar(
     modifier: Modifier = Modifier,
     navController: NavHostController,
 ) {
-    BottomNavigation(
-        modifier = modifier,
-        backgroundColor = MaterialTheme.colors.surface,
-        elevation = 2.dp,
-    ) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentDestination = navBackStackEntry?.destination
+    CompositionLocalProvider(LocalElevationOverlay provides null) {
+        BottomNavigation(
+            modifier = modifier,
+            backgroundColor = MaterialTheme.colors.surface,
+        ) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
-        BottomNavBarItem.items.forEach { item ->
-            BottomNavigationItem(
-                icon = { Icon(imageVector = item.icon, contentDescription = null) },
-                label = { Text(stringResource(item.labelResId)) },
-                selectedContentColor = MaterialTheme.colors.primary,
-                unselectedContentColor = MaterialTheme.colors.onSurface,
-                selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+            BottomNavBarItem.items.forEach { item ->
+                BottomNavigationItem(
+                    icon = { Icon(imageVector = item.icon, contentDescription = null) },
+                    label = { Text(stringResource(item.labelResId)) },
+                    selectedContentColor = MaterialTheme.colors.primary,
+                    unselectedContentColor = MaterialTheme.colors.onSurface,
+                    selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-            )
+                    },
+                )
+            }
         }
     }
 }
