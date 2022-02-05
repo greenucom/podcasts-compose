45bb498ea7e7df7080cccc68b1c46a3ecbdc0f0b
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-05 23:04:58 +0300

    Add LoadingScreen.kt

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/components/LoadingScreen.kt
@@ -0,0 +1,57 @@
+package com.greencom.android.podcasts2.ui.common.components
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
+
+@Composable
+fun LoadingScreen(
+    modifier: Modifier = Modifier,
+    text: String,
+) {
+    Surface(modifier = modifier.fillMaxSize()) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            CircularProgressIndicator()
+
+            Spacer(Modifier.height(8.dp))
+
+            Text(
+                modifier = Modifier.padding(horizontal = 48.dp),
+                text = text,
+                style = MaterialTheme.typography.h6,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun Light() {
+    PodcastsComposeTheme {
+        LoadingScreen(text = "Loading podcasts...")
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    locale = "ru",
+)
+private fun Dark() {
+    PodcastsComposeTheme {
+        LoadingScreen(text = "Загрузка подкастов...")
+    }
+}
