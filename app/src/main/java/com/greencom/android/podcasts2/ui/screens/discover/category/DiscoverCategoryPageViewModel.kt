ef07441df95009bfebd1858a57949699d8c4315e
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-05 23:03:33 +0300

    Add DiscoverCategoryPageViewModel.kt

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/category/DiscoverCategoryPageViewModel.kt
@@ -0,0 +1,48 @@
+package com.greencom.android.podcasts2.ui.screens.discover.category
+
+import androidx.lifecycle.viewModelScope
+import com.greencom.android.podcasts2.domain.categories.TrendingCategory
+import com.greencom.android.podcasts2.domain.podcasts.TrendingPodcast
+import com.greencom.android.podcasts2.domain.podcasts.payload.GetTrendingPodcastsPayload
+import com.greencom.android.podcasts2.domain.podcasts.usecases.GetTrendingPodcastsUseCase
+import com.greencom.android.podcasts2.ui.common.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DiscoverCategoryPageViewModel @Inject constructor(
+    private val getTrendingPodcastsUseCase: GetTrendingPodcastsUseCase,
+) : BaseViewModel() {
+
+    private val _viewState = MutableStateFlow<ViewState>(ViewState.InitialLoading)
+    val viewState = _viewState.asStateFlow()
+
+    private var loadTrendingPodcastsJob: Job? = null
+
+    fun loadTrendingPodcasts(category: TrendingCategory) {
+        loadTrendingPodcastsJob?.cancel()
+        loadTrendingPodcastsJob = viewModelScope.launch {
+            val params = GetTrendingPodcastsPayload(category)
+            getTrendingPodcastsUseCase(params).fold(
+                onSuccess = ::onLoadTrendingPodcastsSuccess,
+                onFailure = { _viewState.update { ViewState.Error } },
+            )
+        }
+    }
+
+    private fun onLoadTrendingPodcastsSuccess(podcasts: List<TrendingPodcast>) {
+        _viewState.update { ViewState.TrendingPodcasts(podcasts) }
+    }
+
+    sealed interface ViewState {
+        object InitialLoading : ViewState
+        data class TrendingPodcasts(val podcasts: List<TrendingPodcast>) : ViewState
+        object Error : ViewState
+    }
+
+}
