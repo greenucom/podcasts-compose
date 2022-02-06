0243e78fc84a8f96f7de30dbda9ce5e559f5ec27
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-06 16:00:16 +0300

    Make IPodcast.kt sealed class

--- app/src/main/java/com/greencom/android/podcasts2/domain/podcasts/IPodcast.kt
+++ app/src/main/java/com/greencom/android/podcasts2/domain/podcasts/IPodcast.kt
@@ -1,6 +1,6 @@
 package com.greencom.android.podcasts2.domain.podcasts
 
-abstract class IPodcast(
+sealed class IPodcast(
     open val id: Long,
     open val title: String,
     open val description: String,
