7b9ed5725ee571adbdd03cfc1e8ed120d2fbf97c
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-06 16:01:25 +0300

    Make ICategory.kt sealed class

--- app/src/main/java/com/greencom/android/podcasts2/domain/categories/ICategory.kt
+++ app/src/main/java/com/greencom/android/podcasts2/domain/categories/ICategory.kt
@@ -1,5 +1,5 @@
 package com.greencom.android.podcasts2.domain.categories
 
-abstract class ICategory(open val id: Int, open val name: String)
+sealed class ICategory(open val id: Int, open val name: String)
 
 fun List<ICategory>.toCategoriesString(): String = this.joinToString(",") { it.name }
