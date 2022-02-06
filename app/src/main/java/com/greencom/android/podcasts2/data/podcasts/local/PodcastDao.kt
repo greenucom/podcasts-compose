f5075f7ff1e42c07d2f0e76da9cec6a7a938708c
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-06 14:43:33 +0300

    Update PodcastDao.kt

--- app/src/main/java/com/greencom/android/podcasts2/data/podcasts/local/PodcastDao.kt
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcasts/local/PodcastDao.kt
@@ -1,8 +1,34 @@
 package com.greencom.android.podcasts2.data.podcasts.local
 
-import androidx.room.Dao
+import androidx.room.*
 
 @Dao
-interface PodcastDao {
+abstract class PodcastDao {
+
+    @Transaction
+    open suspend fun insert(podcasts: List<PodcastEntity>) {
+        insertToTemp(podcasts)
+        mergeTemp()
+        update(podcasts)
+        clearTemp()
+    }
+
+    @Insert(entity = PodcastEntityTemp::class, onConflict = OnConflictStrategy.REPLACE)
+    protected abstract suspend fun insertToTemp(podcasts: List<PodcastEntity>)
+
+    @Query("""
+        INSERT INTO podcasts (id, title, description, author, image, is_subscribed)
+        SELECT tmp.id, tmp.title, tmp.description, tmp.author, tmp.image, tmp.is_subscribed
+        FROM podcasts_temp tmp
+        LEFT JOIN podcasts ON tmp.id = podcasts.id 
+        WHERE podcasts.id IS NULL
+    """)
+    protected abstract suspend fun mergeTemp()
+
+    @Update(entity = PodcastEntity::class)
+    protected abstract suspend fun update(podcasts: List<PodcastEntity>)
+
+    @Query("DELETE FROM podcasts_temp")
+    protected abstract suspend fun clearTemp()
 
 }
