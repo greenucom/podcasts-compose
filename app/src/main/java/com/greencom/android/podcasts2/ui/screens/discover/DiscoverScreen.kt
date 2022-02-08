fb27449e43ba5b404fb66f71b35241a12aa56844
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-08 23:51:46 +0300

    Cleaning

--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverScreen.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverScreen.kt
@@ -1,25 +1,11 @@
 package com.greencom.android.podcasts2.ui.screens.discover
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.greencom.android.podcasts2.R
-import com.greencom.android.podcasts2.domain.podcasts.IPodcast
-import com.greencom.android.podcasts2.ui.common.CrossfadeAnimatedVisibility
-import com.greencom.android.podcasts2.ui.common.components.PodcastItem
-import com.greencom.android.podcasts2.ui.common.screen.LoadingScreen
-import com.greencom.android.podcasts2.ui.screens.discover.DiscoverViewModel.ViewState
 import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
 
 @Composable
@@ -28,31 +14,7 @@ fun DiscoverScreen(
     discoverViewModel: DiscoverViewModel = hiltViewModel(),
 ) {
     Surface(modifier = modifier) {
-        val viewState by discoverViewModel.viewState.collectAsState()
 
-        CrossfadeAnimatedVisibility(viewState is ViewState.InitialLoading) {
-            LoadingScreen(text = stringResource(R.string.loading_podcasts))
-        }
-
-        CrossfadeAnimatedVisibility(viewState is ViewState.PodcastList) {
-            val state = viewState as ViewState.PodcastList
-            LazyColumn(
-                modifier = modifier,
-                contentPadding = PaddingValues(16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                items(items = state.podcasts, key = IPodcast::id) { podcast ->
-                    PodcastItem(
-                        podcast = podcast,
-                        onPodcastClicked = discoverViewModel::onPodcastClicked,
-                    )
-                }
-            }
-        }
-
-        CrossfadeAnimatedVisibility(viewState is ViewState.Error) {
-
-        }
     }
 }
 
--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverViewModel.kt
@@ -1,61 +1,10 @@
 package com.greencom.android.podcasts2.ui.screens.discover
 
-import androidx.lifecycle.viewModelScope
-import com.greencom.android.podcasts2.domain.categories.TrendingCategory
-import com.greencom.android.podcasts2.domain.podcasts.IPodcast
-import com.greencom.android.podcasts2.domain.podcasts.TrendingPodcast
-import com.greencom.android.podcasts2.domain.podcasts.payload.GetTrendingPodcastsPayload
-import com.greencom.android.podcasts2.domain.podcasts.usecases.GetTrendingPodcastsUseCase
 import com.greencom.android.podcasts2.ui.common.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class DiscoverViewModel @Inject constructor(
-    private val getTrendingPodcastsUseCase: GetTrendingPodcastsUseCase,
-) : BaseViewModel() {
-
-    private val _viewState = MutableStateFlow<ViewState>(ViewState.InitialLoading)
-    val viewState = _viewState.asStateFlow()
-
-    private var loadTrendingPodcastsJob: Job? = null
-
-    init {
-        loadTrendingPodcasts()
-    }
-
-    fun onPodcastClicked(podcast: IPodcast) {
-
-    }
-
-    private fun loadTrendingPodcasts() {
-        loadTrendingPodcastsJob?.cancel()
-        loadTrendingPodcastsJob = viewModelScope.launch {
-            val params = GetTrendingPodcastsPayload(
-                inCategories = TrendingCategory.categories,
-                max = MAX_TRENDING_PODCASTS,
-            )
-            getTrendingPodcastsUseCase(params)
-                .onSuccess { podcasts ->
-                    _viewState.update { ViewState.PodcastList(podcasts) }
-                }
-                .onFailure { /* TODO */ }
-        }
-    }
-
-    sealed interface ViewState {
-        object InitialLoading : ViewState
-        data class PodcastList(val podcasts: List<TrendingPodcast>) : ViewState
-        object Error : ViewState
-    }
-
-    companion object {
-        private const val MAX_TRENDING_PODCASTS = 30
-    }
+class DiscoverViewModel @Inject constructor() : BaseViewModel() {
 
 }
