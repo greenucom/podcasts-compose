f6757b78384e2f538e15c5f3e46b5a03554ba4ba
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-10 00:19:14 +0300

    Cleaning

--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverScreen.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverScreen.kt
@@ -1,15 +1,12 @@
 package com.greencom.android.podcasts2.ui.screens.discover
 
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.greencom.android.podcasts2.ui.screens.discover.components.SearchBar
-import com.greencom.android.podcasts2.ui.screens.discover.components.SearchBarUtils
 import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
 
 @Composable
@@ -18,17 +15,7 @@ fun DiscoverScreen(
     discoverViewModel: DiscoverViewModel = hiltViewModel(),
 ) {
     Surface(modifier = modifier.fillMaxSize()) {
-        Column {
-            SearchBar(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(SearchBarUtils.Height)
-                    .padding(horizontal = 16.dp, vertical = 8.dp),
-                value = "TODO",
-                onValueChange = { /* TODO */ },
-                onImeSearch = { /* TODO */ },
-            )
-        }
+
     }
 }
 
