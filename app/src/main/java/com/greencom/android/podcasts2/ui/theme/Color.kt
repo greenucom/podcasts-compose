e63d98cd49fcaa73101929558e189f53b534187e
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-09 23:54:43 +0300

    Update Color.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/theme/Color.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/theme/Color.kt
@@ -1,5 +1,6 @@
 package com.greencom.android.podcasts2.ui.theme
 
+import androidx.compose.material.Colors
 import androidx.compose.ui.graphics.Color
 
 val BluePrimaryLight = Color(0xFF1976D2)
@@ -7,3 +8,8 @@ val BluePrimaryDark = Color(0xFF90CAF9)
 
 val SurfaceLight = Color(0xFFFFFFFF)
 val SurfaceDark = Color(0xFF222222)
+
+val OnSurfaceUtilLight = Color(0xFFE0E0E0)
+val OnSurfaceUtilDark = Color(0xFF3D3D3D)
+val Colors.onSurfaceUtil: Color
+    get() = if (isLight) OnSurfaceUtilLight else OnSurfaceUtilDark
