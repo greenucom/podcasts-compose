04c8665825d0971f7cbc179fee4ebf936475234b
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-09 23:49:13 +0300

    Update DiscoverScreen.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverScreen.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/DiscoverScreen.kt
@@ -1,11 +1,15 @@
 package com.greencom.android.podcasts2.ui.screens.discover
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.greencom.android.podcasts2.ui.screens.discover.components.SearchBar
+import com.greencom.android.podcasts2.ui.screens.discover.components.SearchBarUtils
 import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
 
 @Composable
@@ -13,8 +17,18 @@ fun DiscoverScreen(
     modifier: Modifier = Modifier,
     discoverViewModel: DiscoverViewModel = hiltViewModel(),
 ) {
-    Surface(modifier = modifier) {
-
+    Surface(modifier = modifier.fillMaxSize()) {
+        Column {
+            SearchBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(SearchBarUtils.Height)
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                value = "TODO",
+                onValueChange = { /* TODO */ },
+                onImeSearch = { /* TODO */ },
+            )
+        }
     }
 }
 
--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/components/SearchBarUtils.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/components/SearchBarUtils.kt
@@ -1,6 +1,9 @@
 package com.greencom.android.podcasts2.ui.screens.discover.components
 
+import androidx.compose.ui.unit.dp
+
 object SearchBarUtils {
+    val Height = 64.dp
     const val CornerPercent = 50
     const val DarkBackgroundColorAlpha = 0.08f
 }
