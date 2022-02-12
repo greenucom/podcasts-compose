fbfb997af86f3ac200b5f7f15dd4686c155c8071
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-12 13:08:27 +0300

    Update PodcastCard.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/components/PodcastCard.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/components/PodcastCard.kt
@@ -2,23 +2,18 @@ package com.greencom.android.podcasts2.ui.screens.discover.components
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
+import coil.size.Scale
 import com.greencom.android.podcasts2.R
 import com.greencom.android.podcasts2.domain.podcasts.IPodcast
 import com.greencom.android.podcasts2.domain.podcasts.TrendingPodcast
@@ -33,9 +28,12 @@ fun PodcastCard(
     onPodcastClick: (podcast: IPodcast) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val screenWidthDp = LocalConfiguration.current.screenWidthDp.dp
+    val cardWidthDp = minOf(320.dp, screenWidthDp * 0.8f)
+
     Card(
         modifier = modifier
-            .width(320.dp)
+            .width(cardWidthDp)
             .aspectRatio(1f),
         onClick = { onPodcastClick(podcast) },
         shape = RoundedCornerShape(16.dp),
@@ -43,34 +41,10 @@ fun PodcastCard(
         Box(modifier = Modifier.fillMaxSize()) {
             Image(
                 modifier = Modifier.fillMaxSize(),
-                painter = rememberImagePainterWithCrossfadeAndPlaceholder(podcast.image),
+                painter = rememberImagePainterWithCrossfadeAndPlaceholder(podcast.image) {
+                    scale(Scale.FILL)
+                },
                 contentDescription = stringResource(R.string.podcast_cover),
-            )
-
-            val gradient = Brush.verticalGradient(
-                listOf(
-                    Color.Transparent,
-                    Color.Black.copy(alpha = 0.45f),
-                    Color.Black.copy(alpha = 0.7f),
-                )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .fillMaxHeight(0.4f)
-                    .align(Alignment.BottomCenter)
-                    .background(gradient),
-            )
-
-            Text(
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(16.dp),
-                text = podcast.title,
-                color = Color.White,
-                style = MaterialTheme.typography.h5,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
             )
         }
     }
