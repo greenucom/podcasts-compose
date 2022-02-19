54f5d8e466ea073901be56f2c83c9f24178888e6
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-19 20:00:16 +0300

    Create TrendingPodcasts.kt

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screens/discover/components/TrendingPodcasts.kt
@@ -0,0 +1,36 @@
+package com.greencom.android.podcasts2.ui.screens.discover.components
+
+import android.content.res.Configuration
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
+
+@Composable
+fun TrendingPodcasts() {
+
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun Light() {
+    PodcastsComposeTheme {
+        Surface {
+            TrendingPodcasts()
+        }
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    locale = "ru",
+)
+private fun Dark() {
+    PodcastsComposeTheme {
+        Surface {
+            TrendingPodcasts()
+        }
+    }
+}
