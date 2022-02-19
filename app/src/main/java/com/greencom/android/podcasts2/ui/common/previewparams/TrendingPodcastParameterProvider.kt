046aa438c45ce990c995ef75b15f4c0694cfed68
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-19 19:48:40 +0300

    Create RecommendedPodcasts.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/previewparams/TrendingPodcastParameterProvider.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/previewparams/TrendingPodcastParameterProvider.kt
@@ -6,23 +6,28 @@ import com.greencom.android.podcasts2.domain.podcasts.TrendingPodcast
 class TrendingPodcastParameterProvider : PreviewParameterProvider<TrendingPodcast> {
 
     override val values: Sequence<TrendingPodcast>
-        get() = sequenceOf(short(), long())
+        get() = sequenceOf(shortTrendingPodcast(), longTrendingPodcast())
 
-    private fun short(): TrendingPodcast = TrendingPodcast(
-        id = 1,
-        title = "Lorem Ipsum",
-        description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-        author = "Lorem Ipsum",
-        image = "",
-        isSubscribed = false,
-    )
+    companion object {
 
-    private fun long(): TrendingPodcast = short().copy(
-        title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
-        description = "Lorem Ipsum is simply dummy text of the printing and typesetting " +
-                "industry. Lorem Ipsum has been the industry's standard dummy text ever " +
-                "since the 1500s, when an unknown printer took a galley of type and scrambled " +
-                "it to make a type specimen book.",
-    )
+        fun shortTrendingPodcast(): TrendingPodcast = TrendingPodcast(
+            id = 1,
+            title = "Lorem Ipsum",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+            author = "Lorem Ipsum",
+            image = "",
+            isSubscribed = false,
+        )
+
+        fun longTrendingPodcast(): TrendingPodcast = shortTrendingPodcast().copy(
+            id = 2,
+            title = "Lorem Ipsum is simply dummy text of the printing and typesetting industry",
+            description = "Lorem Ipsum is simply dummy text of the printing and typesetting " +
+                    "industry. Lorem Ipsum has been the industry's standard dummy text ever " +
+                    "since the 1500s, when an unknown printer took a galley of type and scrambled " +
+                    "it to make a type specimen book.",
+        )
+
+    }
 
 }
