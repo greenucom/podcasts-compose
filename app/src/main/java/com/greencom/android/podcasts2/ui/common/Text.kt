08251b85aad23faef7c6bcc27b7b29c1c2efd61a
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-21 00:20:49 +0300

    Add Text.kt

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/Text.kt
@@ -0,0 +1,38 @@
+package com.greencom.android.podcasts2.ui.common
+
+import android.content.Context
+import androidx.annotation.PluralsRes
+import androidx.annotation.StringRes
+
+sealed class Text {
+
+    abstract fun getString(context: Context): String
+
+    object Empty : Text() {
+        override fun getString(context: Context): String = ""
+    }
+
+    class ResourceText(
+        @StringRes val resId: Int,
+        private vararg val args: Any = emptyArray(),
+    ) : Text() {
+        override fun getString(context: Context): String {
+            return context.getString(resId, args)
+        }
+    }
+
+    class PluralResourceText(
+        @PluralsRes val resId: Int,
+        private val count: Int,
+        private vararg val args: Any = emptyArray(),
+    ) : Text() {
+        override fun getString(context: Context): String {
+            return context.resources.getQuantityString(resId, count, args)
+        }
+    }
+
+    class StringText(private val text: String) : Text() {
+        override fun getString(context: Context): String = text
+    }
+
+}
