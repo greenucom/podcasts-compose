ad97b0561ce3225d8a7c92e2ab2010b12e9e18ee
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-21 01:06:41 +0300

    Update DiscoverViewModel.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewModel.kt
@@ -1,10 +1,43 @@
 package com.greencom.android.podcasts2.ui.screen.discover
 
+import com.greencom.android.podcasts2.domain.category.TrendingCategory
+import com.greencom.android.podcasts2.domain.category.usecase.GetTrendingCategoriesUseCase
 import com.greencom.android.podcasts2.ui.common.BaseViewModel
+import com.greencom.android.podcasts2.ui.screen.discover.model.SelectableTrendingCategory
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class DiscoverViewModel @Inject constructor() : BaseViewModel() {
+class DiscoverViewModel @Inject constructor(
+    getTrendingCategoriesUseCase: GetTrendingCategoriesUseCase,
+) : BaseViewModel() {
+
+    private val trendingCategories = MutableStateFlow<List<TrendingCategory>>(
+        getTrendingCategoriesUseCase(Unit)
+    )
+    private val selectedTrendingCategoriesIds = MutableStateFlow<Set<Int>>(
+        emptySet()
+    )
+    val selectableTrendingCategories = combine(
+        trendingCategories,
+        selectedTrendingCategoriesIds
+    ) { categories, selectedCategoriesIds ->
+        categories.map { category ->
+            SelectableTrendingCategory(
+                isSelected = category.id in selectedCategoriesIds,
+                category = category,
+            )
+        }
+    }
+
+    fun onTrendingCategoryClicked(selectableCategory: SelectableTrendingCategory) {
+        val id = selectableCategory.category.id
+        selectedTrendingCategoriesIds.update { set ->
+            if (id in set) set - id else set + id
+        }
+    }
 
 }
