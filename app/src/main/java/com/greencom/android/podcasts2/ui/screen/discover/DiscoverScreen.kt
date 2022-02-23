01a33c94bf7b80eeebb8e15031a32e6583ca8855
Artem Sidorov <s.artem.ekb@gmail.com>
2022-02-23 14:01:01 +0300

    Update DiscoverScreen.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverScreen.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverScreen.kt
@@ -1,19 +1,75 @@
 package com.greencom.android.podcasts2.ui.screen.discover
 
 import android.content.res.Configuration
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.greencom.android.podcasts2.ui.common.component.PodcastItem
+import com.greencom.android.podcasts2.ui.screen.discover.component.TrendingCategorySelector
 import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
+import com.greencom.android.podcasts2.ui.theme.onSurfaceUtil
 
+private const val KeyCategorySelector = "category_selector"
+
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun DiscoverScreen(
     modifier: Modifier = Modifier,
     discoverViewModel: DiscoverViewModel = hiltViewModel(),
 ) {
+    val viewState by discoverViewModel.viewState.collectAsState()
 
+    val trendingCategories by discoverViewModel.selectableTrendingCategories
+        .collectAsState(emptyList())
+
+    val trendingPodcasts = viewState.trendingPodcasts
+    val trendingPodcastsAlpha by animateFloatAsState(
+        if (trendingPodcasts.isNotEmpty()) 1f else 0f
+    )
+
+    LazyColumn(modifier = modifier) {
+
+        stickyHeader(key = KeyCategorySelector) {
+            Surface {
+                TrendingCategorySelector(
+                    categories = trendingCategories,
+                    onCategoryClicked = discoverViewModel::onTrendingCategoryClicked,
+                    contentPadding = PaddingValues(start = 16.dp, end = 16.dp),
+                )
+            }
+        }
+
+        if (trendingPodcasts.isNotEmpty()) {
+            itemsIndexed(
+                items = trendingPodcasts,
+                key = { _, podcast -> podcast.id },
+            ) { index, podcast ->
+                PodcastItem(
+                    modifier = Modifier.alpha(trendingPodcastsAlpha),
+                    podcast = podcast,
+                    onPodcastClicked = { /* TODO */ },
+                )
+
+                if (index != trendingPodcasts.lastIndex) {
+                    Divider(color = MaterialTheme.colors.onSurfaceUtil)
+                }
+            }
+        }
+
+    }
 }
 
 @Composable
--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewModel.kt
@@ -5,12 +5,14 @@ import com.greencom.android.podcasts2.domain.category.TrendingCategory
 import com.greencom.android.podcasts2.domain.category.usecase.GetSelectedTrendingCategoriesIdsUseCase
 import com.greencom.android.podcasts2.domain.category.usecase.GetTrendingCategoriesUseCase
 import com.greencom.android.podcasts2.domain.category.usecase.ToggleSelectedTrendingCategoryIdUseCase
+import com.greencom.android.podcasts2.domain.podcast.TrendingPodcast
+import com.greencom.android.podcasts2.domain.podcast.usecase.GetTrendingPodcastsPayload
+import com.greencom.android.podcasts2.domain.podcast.usecase.GetTrendingPodcastsUseCase
 import com.greencom.android.podcasts2.ui.common.BaseViewModel
 import com.greencom.android.podcasts2.ui.screen.discover.model.SelectableTrendingCategory
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,7 +21,14 @@ class DiscoverViewModel @Inject constructor(
     getTrendingCategoriesUseCase: GetTrendingCategoriesUseCase,
     getSelectedTrendingCategoriesIdsUseCase: GetSelectedTrendingCategoriesIdsUseCase,
     private val toggleSelectedTrendingCategoryIdUseCase: ToggleSelectedTrendingCategoryIdUseCase,
+    private val getTrendingPodcastsUseCase: GetTrendingPodcastsUseCase,
 ) : BaseViewModel() {
+
+    private val _viewState = MutableStateFlow(ViewState())
+    val viewState = _viewState.asStateFlow()
+
+    private var trendingPodcastsJob: Job? = null
+    private var lastSelectedTrendingCategoriesWithSuccessfulLoad: List<TrendingCategory>? = null
 
     private val trendingCategories = MutableStateFlow<List<TrendingCategory>>(
         getTrendingCategoriesUseCase(Unit)
@@ -36,13 +45,57 @@ class DiscoverViewModel @Inject constructor(
                 category = category,
             )
         }
-    }
+    }.onEach(::loadTrendingPodcastsForSelectedCategories)
 
     fun onTrendingCategoryClicked(selectableCategory: SelectableTrendingCategory) {
         viewModelScope.launch {
             val id = selectableCategory.category.id
             toggleSelectedTrendingCategoryIdUseCase(id)
         }
+    }
+
+    private fun loadTrendingPodcastsForSelectedCategories(
+        categories: List<SelectableTrendingCategory>
+    ) {
+        val selectedCategories = categories
+            .filter { it.isSelected }
+            .map { it.category }
+        if (selectedCategories == lastSelectedTrendingCategoriesWithSuccessfulLoad) return
+
+        trendingPodcastsJob?.cancel()
+        trendingPodcastsJob = viewModelScope.launch {
+            val max = if (selectedCategories.isEmpty()) {
+                TrendingPodcastCountMaxValue
+            } else {
+                val value = selectedCategories.size * TrendingPodcastCountPerCategory
+                value.coerceIn(TrendingPodcastCountMinValue, TrendingPodcastCountMaxValue)
+            }
+
+            val params = GetTrendingPodcastsPayload(
+                max = max,
+                inCategories = selectedCategories,
+            )
+            getTrendingPodcastsUseCase(params)
+                .onSuccess(::onLoadTrendingPodcastsSuccess)
+                .onFailure { /* TODO */ }
+
+            lastSelectedTrendingCategoriesWithSuccessfulLoad = selectedCategories
+        }
+    }
+
+    private fun onLoadTrendingPodcastsSuccess(podcasts: List<TrendingPodcast>) {
+        _viewState.update { it.copy(trendingPodcasts = podcasts) }
+    }
+
+    data class ViewState(
+        val trendingCategories: List<SelectableTrendingCategory> = emptyList(),
+        val trendingPodcasts: List<TrendingPodcast> = emptyList(),
+    )
+
+    companion object {
+        private const val TrendingPodcastCountMaxValue = 40
+        private const val TrendingPodcastCountMinValue = 15
+        private const val TrendingPodcastCountPerCategory = 5
     }
 
 }
