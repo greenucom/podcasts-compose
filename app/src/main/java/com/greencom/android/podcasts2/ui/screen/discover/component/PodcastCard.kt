7381fd6ed6a03d1f56d00de874ac499189e29b97
Artem Sidorov <s.artem.ekb@gmail.com>
2022-03-06 21:59:56 +0300

    Update PodcastCard.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/PodcastCard.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/PodcastCard.kt
@@ -3,13 +3,12 @@ package com.greencom.android.podcasts2.ui.screen.discover.component
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
@@ -29,23 +28,36 @@ fun PodcastCard(
     modifier: Modifier = Modifier,
 ) {
     val screenWidthDp = LocalConfiguration.current.screenWidthDp.dp
-    val cardWidthDp = minOf(PodcastCardUtils.MaxWidthDp, screenWidthDp * 0.8f)
+    val cardWidthDp = minOf(
+        PodcastCardUtils.MaxWidthDp,
+        screenWidthDp * PodcastCardUtils.MaxWidthPercentOfScreen,
+    )
 
     Card(
-        modifier = modifier
-            .width(cardWidthDp)
-            .aspectRatio(1f),
+        modifier = modifier.width(cardWidthDp),
         onClick = { onPodcastClicked(podcast) },
         shape = MaterialTheme.shapes.large,
         elevation = 6.dp,
     ) {
-        Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxWidth()) {
             Image(
-                modifier = Modifier.fillMaxSize(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
                 painter = rememberImagePainterCustom(podcast.image) {
                     scale(Scale.FILL)
                 },
                 contentDescription = stringResource(R.string.podcast_cover),
+            )
+
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .padding(top = 8.dp, bottom = 16.dp),
+                text = PodcastCardUtils.handlePodcastTitle(podcast.title),
+                style = MaterialTheme.typography.h6,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis,
             )
         }
     }
@@ -58,11 +70,13 @@ private fun Light(
     podcast: TrendingPodcast
 ) {
     PodcastsComposeTheme {
-        PodcastCard(
-            modifier = Modifier.padding(16.dp),
-            podcast = podcast,
-            onPodcastClicked = {},
-        )
+        Surface {
+            PodcastCard(
+                modifier = Modifier.padding(16.dp),
+                podcast = podcast,
+                onPodcastClicked = {},
+            )
+        }
     }
 }
 
@@ -77,10 +91,12 @@ private fun Dark(
     podcast: TrendingPodcast
 ) {
     PodcastsComposeTheme {
-        PodcastCard(
-            modifier = Modifier.padding(16.dp),
-            podcast = podcast,
-            onPodcastClicked = {},
-        )
+        Surface {
+            PodcastCard(
+                modifier = Modifier.padding(16.dp),
+                podcast = podcast,
+                onPodcastClicked = {},
+            )
+        }
     }
 }
--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/PodcastCardUtils.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/PodcastCardUtils.kt
@@ -1,9 +1,25 @@
 package com.greencom.android.podcasts2.ui.screen.discover.component
 
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 
 object PodcastCardUtils {
 
     val MaxWidthDp = 320.dp
+    const val MaxWidthPercentOfScreen = 0.7f
+
+    private const val TitleSuffix = " tiredtiredtiredtiredtiredtiredtiredtiredtired"
+
+    fun handlePodcastTitle(title: String): AnnotatedString = buildAnnotatedString {
+        append(title)
+
+        withStyle(style = SpanStyle(color = Color.Transparent)) {
+            append(TitleSuffix)
+        }
+    }
 
 }
