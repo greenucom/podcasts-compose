9df3aa042bc46cbe437ab7011efe0d85b7ad287d
Artem Sidorov <s.artem.ekb@gmail.com>
2022-03-07 18:32:25 +0300

    Update TrendingPodcastList.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/TrendingPodcastList.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/TrendingPodcastList.kt
@@ -2,16 +2,16 @@ package com.greencom.android.podcasts2.ui.screen.discover.component
 
 import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -79,7 +79,14 @@ fun LazyListScope.trendingPodcastList(
 
         DiscoverViewModel.TrendingPodcastsState.Loading -> {
             item(key = KeyLoading) {
-                // TODO: Add loading
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    CircularProgressIndicator()
+                }
             }
         }
 
