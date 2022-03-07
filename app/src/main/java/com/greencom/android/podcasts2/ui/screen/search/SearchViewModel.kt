2f90b325dcbdeeb4d25451c19231577a3b12a422
Artem Sidorov <s.artem.ekb@gmail.com>
2022-03-07 23:40:40 +0300

    Create SearchViewModel.kt

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchViewModel.kt
@@ -0,0 +1,10 @@
+package com.greencom.android.podcasts2.ui.screen.search
+
+import com.greencom.android.podcasts2.ui.common.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchViewModel @Inject constructor() : BaseViewModel() {
+
+}
