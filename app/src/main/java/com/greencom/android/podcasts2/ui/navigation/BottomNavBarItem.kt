a4a095d78e03fc32a1895936b933e50c5d6b8bed
Artyom Sidorov <s.artem.ekb@gmail.com>
2022-03-12 14:11:11 +0300

    Update BottomNavBarItem to use Text as label instead of labelResId

--- app/src/main/java/com/greencom/android/podcasts2/ui/navigation/BottomNavBarItem.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/navigation/BottomNavBarItem.kt
@@ -1,34 +1,34 @@
 package com.greencom.android.podcasts2.ui.navigation
 
-import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Apps
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material.icons.outlined.VideoLibrary
 import androidx.compose.ui.graphics.vector.ImageVector
 import com.greencom.android.podcasts2.R
+import com.greencom.android.podcasts2.ui.common.Text
 
 sealed class BottomNavBarItem(
     val route: String,
-    @StringRes val labelResId: Int,
+    val label: Text,
     val icon: ImageVector,
 ) {
 
     object MyPodcasts : BottomNavBarItem(
         route = "myPodcastsGraph",
-        labelResId = R.string.podcasts,
+        label = Text.ResourceText(R.string.podcasts),
         icon = Icons.Outlined.Apps,
     )
 
     object Discover : BottomNavBarItem(
         route = "discoverGraph",
-        labelResId = R.string.discover,
+        label = Text.ResourceText(R.string.discover),
         icon = Icons.Outlined.Search,
     )
 
     object Library : BottomNavBarItem(
         route = "libraryGraph",
-        labelResId = R.string.library,
+        label = Text.ResourceText(R.string.library),
         icon = Icons.Outlined.VideoLibrary,
     )
 
--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/app/component/BottomNavBar.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/app/component/BottomNavBar.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
@@ -64,7 +64,7 @@ fun BottomNavBar(
 
                         BottomNavigationItem(
                             icon = { Icon(imageVector = item.icon, contentDescription = null) },
-                            label = { Text(stringResource(item.labelResId)) },
+                            label = { Text(item.label.getString(LocalContext.current)) },
                             selectedContentColor = MaterialTheme.colors.primary,
                             unselectedContentColor = MaterialTheme.colors.onSurface,
                             selected = isSelected,
