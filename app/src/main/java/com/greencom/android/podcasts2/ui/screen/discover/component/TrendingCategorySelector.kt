299a1b9e290e675af68763c2e3f5deb2f7a128d4
Artem Sidorov <s.artem.ekb@gmail.com>
2022-03-18 22:07:34 +0300

    Create CategoryHelper.kt, update TrendingCategorySelector.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/TrendingCategorySelector.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/component/TrendingCategorySelector.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.greencom.android.podcasts2.domain.category.Category
 import com.greencom.android.podcasts2.ui.common.SelectableItem
+import com.greencom.android.podcasts2.ui.common.category.CategoryHelper
 import com.greencom.android.podcasts2.ui.common.component.FilterChip
 import com.greencom.android.podcasts2.ui.screen.discover.previewparameter.SelectableTrendingCategoriesParameterProvider
 import com.greencom.android.podcasts2.ui.theme.PodcastsComposeTheme
@@ -33,11 +35,17 @@ fun TrendingCategorySelector(
                 items = selectableCategories,
                 key = { it.item.id },
             ) { selectableCategory ->
-                FilterChip(
-                    isSelected = selectableCategory.isSelected,
-                    onSelectedChanged = { onSelectableCategoryClicked(selectableCategory) },
-                    text = selectableCategory.item.name,
-                )
+                val category = selectableCategory.item
+                val context = LocalContext.current
+                val localizedName = CategoryHelper.getLocalizedName(category, context)
+
+                if (localizedName != null) {
+                    FilterChip(
+                        isSelected = selectableCategory.isSelected,
+                        onSelectedChanged = { onSelectableCategoryClicked(selectableCategory) },
+                        text = selectableCategory.item.name,
+                    )
+                }
             }
         }
     }
