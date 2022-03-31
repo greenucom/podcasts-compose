0057764b9b957d33eecf73dff5730afdb29af2aa
Artyom Sidorov <s.artem.ekb@gmail.com>
2022-03-31 22:22:24 +0300

    Update NavBackStackEntry.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/NavBackStackEntry.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/NavBackStackEntry.kt
@@ -2,6 +2,14 @@ package com.greencom.android.podcasts2.ui.common
 
 import androidx.navigation.NavBackStackEntry
 
+fun NavBackStackEntry.requireString(key: String): String {
+    return requireNotNull(this.arguments?.getString(key))
+}
+
+fun NavBackStackEntry.requireInt(key: String): Int {
+    return requireNotNull(this.arguments?.getInt(key))
+}
+
 fun NavBackStackEntry.requireLong(key: String): Long {
     return requireNotNull(this.arguments?.getLong(key))
 }
