eb4a54a8fabde97f5052225b33281bc12c1ed6c1
Artyom Sidorov <s.artem.ekb@gmail.com>
2022-04-09 15:48:00 +0300

    Collect search-podcasts-related in SearchViewModel

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchInteractor.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchInteractor.kt
@@ -1,5 +1,10 @@
 package com.greencom.android.podcasts2.ui.screen.search
 
+import com.greencom.android.podcasts2.domain.podcast.usecase.GetSearchPodcastsResultFlowUseCase
+import com.greencom.android.podcasts2.domain.podcast.usecase.SearchPodcastsUseCase
 import javax.inject.Inject
 
-class SearchInteractor @Inject constructor()
+class SearchInteractor @Inject constructor(
+    val searchPodcastsUseCase: SearchPodcastsUseCase,
+    val getSearchPodcastsResultFlowUseCase: GetSearchPodcastsResultFlowUseCase,
+)
--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchViewModel.kt
@@ -1,10 +1,13 @@
 package com.greencom.android.podcasts2.ui.screen.search
 
+import androidx.lifecycle.viewModelScope
+import com.greencom.android.podcasts2.domain.podcast.Podcast
 import com.greencom.android.podcasts2.ui.common.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,10 +15,22 @@ class SearchViewModel @Inject constructor(
     private val interactor: SearchInteractor,
 ) : BaseViewModel<SearchViewModel.ViewState, SearchViewModel.ViewEvent>() {
 
-    override val initialViewState = ViewState.None
+    override val initialViewState = ViewState.Empty
 
     private val _query = MutableStateFlow("")
     val query = _query.asStateFlow()
+
+    init {
+        collectSearchResults()
+    }
+
+    private fun collectSearchResults() = viewModelScope.launch {
+        interactor.getSearchPodcastsResultFlowUseCase(Unit).collect { podcasts ->
+            _viewState.update {
+                if (podcasts.isNotEmpty()) ViewState.Success(podcasts) else ViewState.Empty
+            }
+        }
+    }
 
     fun onQueryChange(query: String) {
         _query.update { query }
@@ -30,7 +45,10 @@ class SearchViewModel @Inject constructor(
     }
 
     sealed interface ViewState {
-        object None : ViewState
+        data class Success(val podcasts: List<Podcast>) : ViewState
+        object Loading : ViewState
+        object Empty : ViewState
+        object Error : ViewState
     }
 
     sealed interface ViewEvent
