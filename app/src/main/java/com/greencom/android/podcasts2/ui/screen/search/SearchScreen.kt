ed923642a7822a6951b002859f792cc3d3a51916
Artem Sidorov <s.artem.ekb@gmail.com>
2022-04-09 23:48:51 +0300

    Add placeholder when the search query is blank

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchScreen.kt
@@ -1,6 +1,7 @@
 package com.greencom.android.podcasts2.ui.screen.search
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -11,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.greencom.android.podcasts2.domain.podcast.Podcast
 import com.greencom.android.podcasts2.ui.common.animatePlaceholderLoadingEffectColor
@@ -18,10 +20,12 @@ import com.greencom.android.podcasts2.ui.common.applyAppContentPaddings
 import com.greencom.android.podcasts2.ui.common.component.ErrorMessage
 import com.greencom.android.podcasts2.ui.common.component.PodcastItem
 import com.greencom.android.podcasts2.ui.common.component.PodcastItemPlaceholder
+import com.greencom.android.podcasts2.ui.screen.search.component.SearchEmptyMessage
 import com.greencom.android.podcasts2.ui.screen.search.component.SearchTopBar
 import com.greencom.android.podcasts2.ui.theme.onSurfaceUtil
 
 private const val KeyLoading = "Loading"
+private const val KeyEmpty = "Empty"
 private const val KeyError = "Error"
 
 private const val LoadingPlaceholderCount = 6
@@ -90,7 +94,11 @@ fun SearchScreen(
                         }
                     }
 
-                    SearchViewModel.ViewState.Empty -> {}
+                    SearchViewModel.ViewState.Empty -> {
+                        item(key = KeyEmpty) {
+                            SearchEmptyMessage(modifier = Modifier.padding(top = 32.dp))
+                        }
+                    }
 
                     SearchViewModel.ViewState.Error -> {
                         item(key = KeyError) {
