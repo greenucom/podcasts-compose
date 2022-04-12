afcb717f04bc2102ebbec3bb59fd55dad971f2c6
Artem Sidorov <s.artem.ekb@gmail.com>
2022-04-12 12:00:39 +0300

    Add placeholders for Coil

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/Coil.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/Coil.kt
@@ -1,11 +1,13 @@
 package com.greencom.android.podcasts2.ui.common
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.greencom.android.podcasts2.R
 
 @Composable
 fun AsyncImageCustom(
@@ -14,12 +16,20 @@ fun AsyncImageCustom(
     contentDescription: String?,
     contentScale: ContentScale = ContentScale.Crop,
 ) {
-    // TODO: Add placeholder, error and fallback
+    val placeholderResId = if (isSystemInDarkTheme()) {
+        R.drawable.img_image_placeholder_dark
+    } else {
+        R.drawable.img_image_placeholder_light
+    }
+
     AsyncImage(
         modifier = modifier,
         model = ImageRequest.Builder(LocalContext.current)
             .data(data)
             .crossfade(true)
+            .placeholder(placeholderResId)
+            .error(placeholderResId)
+            .fallback(placeholderResId)
             .build(),
         contentDescription = contentDescription,
         contentScale = contentScale,
