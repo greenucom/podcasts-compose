fda37b2b3627e3f32e33297ccdbe6b0c32c250d1
Artem Sidorov <s.artem.ekb@gmail.com>
2022-04-15 23:35:13 +0300

    Create SizeTypeConverter.kt

---
+++ app/src/main/java/com/greencom/android/podcasts2/data/common/SizeTypeConverter.kt
@@ -0,0 +1,15 @@
+package com.greencom.android.podcasts2.data.common
+
+import androidx.room.TypeConverter
+import com.greencom.android.podcasts2.utils.Size
+import com.greencom.android.podcasts2.utils.Size.Companion.bytes
+
+class SizeTypeConverter {
+
+    @TypeConverter
+    fun fromSize(size: Size): Long = size.inBytes
+
+    @TypeConverter
+    fun toSize(value: Long): Size = value.bytes
+
+}
