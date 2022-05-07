9b9adc3e6964fe6447fcbdeeef43258a24036f04
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-07 21:59:17 +0300

    Add MVI base components

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/mvi/Model.kt
@@ -0,0 +1,10 @@
+package com.greencom.android.podcasts2.ui.common.mvi
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+interface Model<ViewState : State, UserIntent : Intent, ViewSideEffect : SideEffect> {
+    val state: StateFlow<ViewState>
+    val sideEffects: Flow<ViewSideEffect>
+    suspend fun handleIntent(event: UserIntent)
+}
---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/mvi/MviViewModel.kt
@@ -0,0 +1,28 @@
+package com.greencom.android.podcasts2.ui.common.mvi
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+
+abstract class MviViewModel<ViewState : State, UserIntent : Intent, ViewSideEffect : SideEffect> :
+    ViewModel(), Model<ViewState, UserIntent, ViewSideEffect> {
+
+    protected abstract val initialViewState: ViewState
+
+    private val _state by lazy { MutableStateFlow(initialViewState) }
+    override val state by lazy { _state }
+
+    private val _sideEffects = Channel<ViewSideEffect>(Channel.UNLIMITED)
+    override val sideEffects = _sideEffects.receiveAsFlow()
+
+    protected fun updateState(function: (ViewState) -> ViewState) {
+        _state.update(function)
+    }
+
+    protected fun emitSideEffect(sideEffect: ViewSideEffect) {
+        _sideEffects.trySend(sideEffect)
+    }
+
+}
