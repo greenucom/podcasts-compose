bcd49540acd04e34f75f71975421a3e83202e54f
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-08 16:49:32 +0300

    Mark MVI state classes as @Immutable

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewState.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewState.kt
@@ -1,10 +1,12 @@
 package com.greencom.android.podcasts2.ui.screen.discover
 
+import androidx.compose.runtime.Immutable
 import com.greencom.android.podcasts2.ui.common.SelectableItem
 import com.greencom.android.podcasts2.ui.common.model.category.CategoryUiModel
 import com.greencom.android.podcasts2.ui.common.model.podcast.PodcastUiModel
 import com.greencom.android.podcasts2.ui.common.mvi.State
 
+@Immutable
 data class DiscoverViewState(
     val recommendedPodcastsState: DiscoverRecommendedPodcastsState =
         DiscoverRecommendedPodcastsState.Loading,
@@ -15,13 +17,27 @@ data class DiscoverViewState(
     val showSearchScreen: Boolean = false,
 ) : State
 
+@Immutable
 sealed interface DiscoverRecommendedPodcastsState {
+
+    @Immutable
     object Loading : DiscoverRecommendedPodcastsState
+
+    @Immutable
     data class Success(val podcasts: List<PodcastUiModel>) : DiscoverRecommendedPodcastsState
+
 }
 
+@Immutable
 sealed interface DiscoverTrendingPodcastsState {
+
+    @Immutable
     object Loading : DiscoverTrendingPodcastsState
+
+    @Immutable
     data class Success(val podcasts: List<PodcastUiModel>) : DiscoverTrendingPodcastsState
+
+    @Immutable
     object Error : DiscoverTrendingPodcastsState
+
 }
--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchViewState.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/search/SearchViewState.kt
@@ -1,8 +1,10 @@
 package com.greencom.android.podcasts2.ui.screen.search
 
+import androidx.compose.runtime.Immutable
 import com.greencom.android.podcasts2.ui.common.model.podcast.PodcastUiModel
 import com.greencom.android.podcasts2.ui.common.mvi.State
 
+@Immutable
 data class SearchViewState(
     val query: String = QueryDefaultValue,
     val resultState: SearchResultState = SearchResultState.Empty,
@@ -15,9 +17,19 @@ data class SearchViewState(
 
 }
 
+@Immutable
 sealed interface SearchResultState {
+
+    @Immutable
     object Empty : SearchResultState
+
+    @Immutable
     object Loading : SearchResultState
+
+    @Immutable
     data class Success(val podcasts: List<PodcastUiModel>) : SearchResultState
+
+    @Immutable
     object Error : SearchResultState
+
 }
