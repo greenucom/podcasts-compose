d684aedbeab1e1e0e9a3dac28d79e197a6f03315
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-18 21:07:19 +0300

    Create PodcastRepository

---
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcast/PodcastRepository.kt
@@ -0,0 +1,10 @@
+package com.greencom.android.podcasts2.data.podcast
+
+import javax.inject.Inject
+
+class PodcastRepository @Inject constructor(
+    private val localDataSource: PodcastLocalDataSource,
+    private val remoteDataSource: PodcastRemoteDataSource,
+) {
+
+}
