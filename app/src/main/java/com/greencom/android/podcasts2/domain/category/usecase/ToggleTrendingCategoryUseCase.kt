1c1faed109352b84420212ca9e88f33b2b78796e
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-20 00:05:30 +0300

    Create ToggleTrendingCategoryUseCase

---
+++ app/src/main/java/com/greencom/android/podcasts2/domain/category/usecase/ToggleTrendingCategoryUseCase.kt
@@ -0,0 +1,19 @@
+package com.greencom.android.podcasts2.domain.category.usecase
+
+import com.greencom.android.podcasts2.base.clean.UseCase
+import com.greencom.android.podcasts2.data.category.CategoryRepository
+import com.greencom.android.podcasts2.di.IODispatcher
+import com.greencom.android.podcasts2.domain.category.Category
+import kotlinx.coroutines.CoroutineDispatcher
+import javax.inject.Inject
+
+class ToggleTrendingCategoryUseCase @Inject constructor(
+    @IODispatcher dispatcher: CoroutineDispatcher,
+    private val categoryRepository: CategoryRepository,
+) : UseCase<Category, Unit>(dispatcher) {
+
+    override suspend fun execute(params: Category) {
+        categoryRepository.toggleTrendingCategory(params)
+    }
+
+}
