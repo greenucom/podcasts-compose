6e4ce5b48db78e8694d0b634cf24334d43980028
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-19 22:43:59 +0300

    Update PodcastDao
    Add ability to insert podcasts to the Podcasts table with PodcastsTemp table.
    
    Add ability to get user's subscriptions IDs.

--- app/src/main/java/com/greencom/android/podcasts2/data/podcast/local/PodcastDao.kt
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcast/local/PodcastDao.kt
@@ -1,8 +1,55 @@
 package com.greencom.android.podcasts2.data.podcast.local
 
-import androidx.room.Dao
+import androidx.room.*
 
 @Dao
-interface PodcastDao {
+abstract class PodcastDao {
+
+    @Transaction
+    open suspend fun insert(podcast: PodcastEntity) {
+        insertToTemp(podcast)
+        mergeTemp()
+        update(podcast)
+        clearTemp()
+    }
+
+    @Transaction
+    open suspend fun insert(podcasts: List<PodcastEntity>) {
+        insertToTemp(podcasts)
+        mergeTemp()
+        update(podcasts)
+        clearTemp()
+    }
+
+    @Query("SELECT podcast_id FROM Podcasts WHERE podcast_is_user_subscribed = 1")
+    abstract suspend fun getUserSubscriptionsIds(): List<Long>
+
+    @Insert(entity = PodcastEntityTemp::class, onConflict = OnConflictStrategy.REPLACE)
+    protected abstract suspend fun insertToTemp(podcast: PodcastEntity)
+
+    @Insert(entity = PodcastEntityTemp::class, onConflict = OnConflictStrategy.REPLACE)
+    protected abstract suspend fun insertToTemp(podcasts: List<PodcastEntity>)
+
+    @Query("""
+        INSERT INTO 
+            Podcasts (podcast_id, podcast_title, podcast_description, podcast_author, 
+                podcast_image_url, podcast_categories, podcast_is_user_subscribed)
+        SELECT 
+            Tmp.podcast_id, Tmp.podcast_title, Tmp.podcast_description, Tmp.podcast_author,
+                Tmp.podcast_image_url, Tmp.podcast_categories, Tmp.podcast_is_user_subscribed
+        FROM PodcastsTemp Tmp
+        LEFT JOIN Podcasts ON Tmp.podcast_id = Podcasts.podcast_id
+        WHERE Podcasts.podcast_id IS NULL
+    """)
+    protected abstract suspend fun mergeTemp()
+
+    @Update
+    protected abstract suspend fun update(podcast: PodcastEntity)
+
+    @Update
+    protected abstract suspend fun update(podcasts: List<PodcastEntity>)
+
+    @Query("DELETE FROM PodcastsTemp")
+    protected abstract suspend fun clearTemp()
 
 }
