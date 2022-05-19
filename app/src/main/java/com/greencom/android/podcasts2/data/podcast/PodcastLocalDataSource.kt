352c7f27023efaba9d7975683683ec5cf3989f9a
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-19 22:51:59 +0300

    Update PodcastLocalDataSource
    Load user's subscriptions IDs at class initialization and store them in the StateFlow

--- app/src/main/java/com/greencom/android/podcasts2/data/podcast/PodcastLocalDataSource.kt
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcast/PodcastLocalDataSource.kt
@@ -1,10 +1,34 @@
 package com.greencom.android.podcasts2.data.podcast
 
 import com.greencom.android.podcasts2.data.podcast.local.PodcastDao
+import com.greencom.android.podcasts2.di.ApplicationScope
+import com.greencom.android.podcasts2.di.IODispatcher
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class PodcastLocalDataSource @Inject constructor(
     private val dao: PodcastDao,
+    @ApplicationScope private val applicationScope: CoroutineScope,
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher,
 ) {
+
+    private val _userSubscriptionsIds = MutableStateFlow(setOf<Long>())
+    val userSubscriptionsIds = _userSubscriptionsIds.asStateFlow()
+
+    init {
+        loadUserSubscriptionsIds()
+    }
+
+    private fun loadUserSubscriptionsIds() = applicationScope.launch(ioDispatcher) {
+        val userSubscriptionsIds = dao.getUserSubscriptionsIds().toSet()
+        _userSubscriptionsIds.update { userSubscriptionsIds }
+    }
 
 }
