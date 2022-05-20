99680ca4dd7baf88c8c4db84c903cfe29f6889eb
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-20 20:20:13 +0300

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -11,11 +11,11 @@ object Versions {
 
     const val coroutines = "1.6.1"
 
-    const val compose = "1.2.0-beta01"
-    const val composeMaterial3 = "1.0.0-alpha11"
+    const val compose = "1.2.0-beta02"
+    const val composeMaterial3 = "1.0.0-alpha12"
 
-    const val core = "1.8.0-rc01"
-    const val coreSplashScreen = "1.0.0-beta02"
+    const val core = "1.8.0-rc02"
+    const val coreSplashScreen = "1.0.0-rc01"
     const val appCompat = "1.4.1"
     const val activity = "1.4.0"
 
