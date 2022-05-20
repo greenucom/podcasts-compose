3a9e01256b3f4cb9a783cdca8dd61c54d1b2170d
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-20 21:06:34 +0300

    Create ScreenBehavior
    ScreenBehavior is a mechanism that allows screen to specify global behavior of app that should be performed when the screen is active

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorEffect.kt
@@ -0,0 +1,26 @@
+package com.greencom.android.podcasts2.ui.common.screenbehavior
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+
+@Composable
+fun SpecificScreenBehavior(builderAction: (ScreenBehaviorBuilder.() -> Unit)) {
+    val screenBehaviorController = LocalScreenBehaviorController.current
+    DisposableEffect(Unit) {
+        val screenBehavior = buildScreenBehavior(builderAction)
+        screenBehaviorController.setScreenBehavior(screenBehavior)
+
+        onDispose { screenBehaviorController.removeScreenBehavior(screenBehavior) }
+    }
+}
+
+@Composable
+fun DefaultScreenBehavior() {
+    val screenBehaviorController = LocalScreenBehaviorController.current
+    DisposableEffect(Unit) {
+        val defaultScreenBehavior = ScreenBehavior.Default
+        screenBehaviorController.setScreenBehavior(defaultScreenBehavior)
+
+        onDispose { screenBehaviorController.removeScreenBehavior(defaultScreenBehavior) }
+    }
+}
