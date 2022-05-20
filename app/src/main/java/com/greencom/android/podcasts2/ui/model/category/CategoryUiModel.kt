eda0836487c98c8dcaf5592d097f0f73790ff409
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-20 21:20:43 +0300

    Update CategoryUiModel

--- app/src/main/java/com/greencom/android/podcasts2/ui/model/category/CategoryUiModel.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/model/category/CategoryUiModel.kt
@@ -1,11 +1,9 @@
 package com.greencom.android.podcasts2.ui.model.category
 
-import androidx.annotation.StringRes
 import androidx.compose.runtime.Immutable
 
 @Immutable
 data class CategoryUiModel(
     val id: Int,
     val name: String,
-    @StringRes val displayNameResId: Int,
 )
