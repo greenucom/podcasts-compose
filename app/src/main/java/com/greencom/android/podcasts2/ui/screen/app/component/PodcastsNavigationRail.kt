e7dbccfebed023e5c1060e34f73cde86558f2a51
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-21 23:40:43 +0300

    Update PodcastsNavigationRail

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/app/component/PodcastsNavigationRail.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/app/component/PodcastsNavigationRail.kt
@@ -2,48 +2,67 @@ package com.greencom.android.podcasts2.ui.screen.app.component
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.NavigationRail
-import androidx.compose.material.NavigationRailItem
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.greencom.android.podcasts2.ui.navigation.NavigationItems
 import com.greencom.android.podcasts2.ui.theme.PodcastsTheme
+import com.greencom.android.podcasts2.ui.theme.onSurfaceUtil
 
 @Composable
 fun PodcastsNavigationRail(
     navController: NavHostController,
     modifier: Modifier = Modifier,
 ) {
-    NavigationRail(modifier = modifier) {
-        val navBackStackEntry by navController.currentBackStackEntryAsState()
-        val currentDestination = navBackStackEntry?.destination
+    Row {
+        NavigationRail(
+            modifier = modifier,
+            backgroundColor = MaterialTheme.colors.surface,
+            elevation = 0.dp,
+        ) {
+            val navBackStackEntry by navController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
 
-        NavigationItems.forEach { item ->
-            val isSelected = currentDestination?.hierarchy
-                ?.any { it.route == item.route } == true
+            NavigationItems.forEach { item ->
+                val isSelected = currentDestination?.hierarchy
+                    ?.any { it.route == item.route } == true
 
-            NavigationRailItem(
-                selected = isSelected,
-                onClick = {
-                    NavigationItemUtils.onNavigationItemClicked(
-                        item = item,
-                        navController = navController,
-                    )
-                },
-                icon = {
-                    NavigationItemUtils.NavigationItemIcon(item = item, isSelected = isSelected)
-                },
-                label = { Text(text = stringResource(id = item.labelResId)) },
-            )
+                NavigationRailItem(
+                    selected = isSelected,
+                    onClick = {
+                        NavigationItemUtils.onNavigationItemClicked(
+                            item = item,
+                            navController = navController,
+                        )
+                    },
+                    icon = {
+                        NavigationItemUtils.NavigationItemIcon(item = item, isSelected = isSelected)
+                    },
+                    label = { Text(text = stringResource(id = item.labelResId)) },
+                    alwaysShowLabel = false,
+                    selectedContentColor = MaterialTheme.colors.primary,
+                    unselectedContentColor = MaterialTheme.colors.onSurface
+                        .copy(alpha = ContentAlpha.high),
+                )
+            }
         }
+
+        val dividerColor = MaterialTheme.colors.onSurfaceUtil
+        Spacer(
+            modifier = Modifier
+                .width(1.dp)
+                .fillMaxHeight()
+                .drawBehind { drawRect(color = dividerColor) }
+        )
     }
 }
 
