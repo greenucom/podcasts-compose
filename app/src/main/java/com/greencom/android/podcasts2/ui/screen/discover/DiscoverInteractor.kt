53996291635eb67a4f7444dce7fb008ce5a05558
Artem Sidorov <s.artem.ekb@gmail.com>
2022-05-28 11:19:55 +0300

    Update DiscoverViewModel

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverInteractor.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverInteractor.kt
@@ -1,10 +1,12 @@
 package com.greencom.android.podcasts2.ui.screen.discover
 
 import com.greencom.android.podcasts2.domain.category.usecase.GetSelectableTrendingCategoriesUseCase
-import com.greencom.android.podcasts2.domain.podcast.usecase.GetTrendingPodcastsUseCase
+import com.greencom.android.podcasts2.domain.category.usecase.ToggleSelectableTrendingCategoryUseCase
+import com.greencom.android.podcasts2.domain.podcast.usecase.GetTrendingPodcastsForSelectedTrendingCategoriesUseCase
 import javax.inject.Inject
 
 class DiscoverInteractor @Inject constructor(
-    val getTrendingPodcasts: GetTrendingPodcastsUseCase,
     val getSelectableTrendingCategories: GetSelectableTrendingCategoriesUseCase,
+    val getTrendingPodcastsForSelectedTrendingCategories: GetTrendingPodcastsForSelectedTrendingCategoriesUseCase,
+    val toggleSelectableTrendingCategory: ToggleSelectableTrendingCategoryUseCase,
 )
--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewModel.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverViewModel.kt
@@ -3,15 +3,11 @@ package com.greencom.android.podcasts2.ui.screen.discover
 import androidx.lifecycle.viewModelScope
 import com.greencom.android.podcasts2.domain.category.Category
 import com.greencom.android.podcasts2.domain.podcast.Podcast
-import com.greencom.android.podcasts2.domain.podcast.usecase.GetTrendingPodcastsUseCase
 import com.greencom.android.podcasts2.ui.common.SelectableItem
 import com.greencom.android.podcasts2.ui.common.mvi.MviViewModel
 import com.greencom.android.podcasts2.ui.model.category.CategoryUiModel
 import com.greencom.android.podcasts2.ui.model.podcast.PodcastUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.getAndUpdate
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,10 +18,9 @@ class DiscoverViewModel @Inject constructor(
 
     override val initialViewState = DiscoverViewState.InitialLoading
 
-    private val collectTrendingPodcastsJob = MutableStateFlow<Job?>(null)
-
     init {
         collectSelectableTrendingCategories()
+        collectTrendingPodcastsForSelectedTrendingCategories()
     }
 
     override suspend fun handleEvent(event: DiscoverViewEvent) {
@@ -40,20 +35,32 @@ class DiscoverViewModel @Inject constructor(
         }
     }
 
+    private fun collectTrendingPodcastsForSelectedTrendingCategories() = viewModelScope.launch {
+        interactor.getTrendingPodcastsForSelectedTrendingCategories(Unit).collect { result ->
+            result
+                .onSuccess(::onCollectTrendingPodcastsForSelectedTrendingCategoriesSuccess)
+                .onFailure(::onCollectTrendingPodcastsForSelectedTrendingCategoriesFailure)
+        }
+    }
+
     private fun onCollectSelectableTrendingCategoriesSuccess(
         selectableTrendingCategories: List<SelectableItem<Category>>,
     ) {
         val selectableTrendingCategoriesUiModels =
             mapSelectableTrendingCategoriesToUiModels(selectableTrendingCategories)
         updateStateWithSelectableTrendingCategories(selectableTrendingCategoriesUiModels)
-
-        val selectedTrendingCategories = selectableTrendingCategories
-            .filter { it.isSelected }
-            .map { it.item }
-        collectTrendingPodcastsForSelectedTrendingCategories(selectedTrendingCategories)
     }
 
     private fun onCollectSelectableTrendingCategoriesFailure(t: Throwable) {
+        TODO("Not yet implemented")
+    }
+
+    private fun onCollectTrendingPodcastsForSelectedTrendingCategoriesSuccess(podcasts: List<Podcast>) {
+        val trendingPodcastsUiModels = podcasts.map { PodcastUiModel.fromPodcast(it) }
+        TODO("Not yet implemented")
+    }
+
+    private fun onCollectTrendingPodcastsForSelectedTrendingCategoriesFailure(t: Throwable) {
         TODO("Not yet implemented")
     }
 
@@ -82,52 +89,6 @@ class DiscoverViewModel @Inject constructor(
                 )
             }
         }
-    }
-
-    private fun collectTrendingPodcastsForSelectedTrendingCategories(
-        selectedTrendingCategories: List<Category>,
-    ) {
-        collectTrendingPodcastsJob.getAndUpdate {
-            viewModelScope.launch {
-                val params = createGetTrendingPodcastsParams(selectedTrendingCategories)
-                interactor.getTrendingPodcasts(params).collect { result ->
-                    result
-                        .onSuccess(::onCollectTrendingPodcastsSuccess)
-                        .onFailure(::onCollectTrendingPodcastsFailure)
-                }
-            }
-        }?.cancel()
-    }
-
-    private fun createGetTrendingPodcastsParams(
-        selectedTrendingCategories: List<Category>,
-    ): GetTrendingPodcastsUseCase.Params {
-        val max = if (selectedTrendingCategories.isNotEmpty()) {
-            (selectedTrendingCategories.size * TrendingPodcastsCountPerCategory)
-                .coerceIn(TrendingPodcastsMinSize, TrendingPodcastsMaxSize)
-        } else {
-            TrendingPodcastsMaxSize
-        }
-
-        return GetTrendingPodcastsUseCase.Params(
-            max = max,
-            inCategories = selectedTrendingCategories,
-        )
-    }
-
-    private fun onCollectTrendingPodcastsSuccess(podcasts: List<Podcast>) {
-        val podcastsUiModels = podcasts.map { PodcastUiModel.fromPodcast(it) }
-        TODO("Not yet implemented")
-    }
-
-    private fun onCollectTrendingPodcastsFailure(t: Throwable) {
-        TODO("Not yet implemented")
-    }
-
-    companion object {
-        private const val TrendingPodcastsMinSize = 10
-        private const val TrendingPodcastsMaxSize = 50
-        private const val TrendingPodcastsCountPerCategory = 5
     }
 
 }
