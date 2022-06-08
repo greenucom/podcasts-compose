7c3c130f8c06ccfbcd9e996da89b471e7da88347
Artem Sidorov <s.artem.ekb@gmail.com>
2022-06-08 16:54:32 +0300

    Update Shape.kt

--- app/src/main/java/com/greencom/android/podcasts2/ui/theme/Shape.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/theme/Shape.kt
@@ -1,5 +1,11 @@
 package com.greencom.android.podcasts2.ui.theme
 
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
 
-val Shapes = Shapes()
+val Shapes = Shapes(
+    small = RoundedCornerShape(percent = 50),
+    medium = RoundedCornerShape(8.dp),
+    large = RoundedCornerShape(16.dp),
+)
