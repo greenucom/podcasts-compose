40336933e51f75ca86fa4fd4c9f77f5c64c82a88
Artem Sidorov <s.artem.ekb@gmail.com>
2022-06-28 20:36:58 +0300

    Update DiscoverScreen

--- app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverScreen.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/screen/discover/DiscoverScreen.kt
@@ -1,7 +1,9 @@
 package com.greencom.android.podcasts2.ui.screen.discover
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -10,7 +12,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.greencom.android.podcasts2.ui.common.CrossfadeStable
 import com.greencom.android.podcasts2.ui.screen.discover.component.SearchPodcastsButton
+import com.greencom.android.podcasts2.ui.screen.discover.component.TrendingCategorySelector
+
+private const val KeyTrendingCategorySelector = "KeyTrendingCategorySelector"
+
+private const val ContentTypeTrendingCategorySelector = "ContentTypeTrendingCategorySelector"
 
 @Composable
 fun DiscoverScreen(
@@ -20,6 +28,40 @@ fun DiscoverScreen(
 ) {
     val screenState = rememberDiscoverScreenState()
     val viewState by viewModel.state.collectAsState()
+
+    Scaffold(
+        modifier = modifier,
+        scaffoldState = screenState.scaffoldState,
+        topBar = { DiscoverTopBar(onSearchPodcastsClicked = onSearchPodcastsClicked) },
+    ) { paddingValues ->
+
+        CrossfadeStable(
+            modifier = Modifier.padding(paddingValues),
+            targetState = viewState,
+        ) { state ->
+            when (state) {
+                DiscoverViewModel.ViewState.InitialLoading -> {}
+
+                is DiscoverViewModel.ViewState.Success -> {
+                    LazyColumn(state = screenState.lazyColumnState) {
+                        item(
+                            key = KeyTrendingCategorySelector,
+                            contentType = ContentTypeTrendingCategorySelector,
+                        ) {
+                            TrendingCategorySelector(
+                                selectableTrendingCategories = state.selectableTrendingCategories,
+                                onCategoryClicked = {
+                                    val event = DiscoverViewModel.ViewEvent.ToggleSelectableTrendingCategory(it)
+                                    viewModel.dispatchEvent(event)
+                                },
+                                contentPadding = PaddingValues(horizontal = 16.dp),
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 @Composable
@@ -28,7 +70,6 @@ private fun DiscoverTopBar(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier) {
-
         val surfaceColor = MaterialTheme.colors.surface
         Spacer(
             modifier = Modifier
