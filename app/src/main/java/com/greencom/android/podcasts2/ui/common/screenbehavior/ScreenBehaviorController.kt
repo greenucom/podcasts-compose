9916569ef321105a5af1d3625e9d1b167054eeff
Artem Sidorov <s.artem.ekb@gmail.com>
2022-06-30 22:37:57 +0300

    Change ScreenBehaviorController.currentScreenBehavior property to currentScreenBehaviorAsState

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorController.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorController.kt
@@ -1,9 +1,10 @@
 package com.greencom.android.podcasts2.ui.common.screenbehavior
 
+import androidx.compose.runtime.State
 import androidx.compose.runtime.staticCompositionLocalOf
 
 interface ScreenBehaviorController {
-    val currentScreenBehavior: ScreenBehavior
+    val currentScreenBehaviorAsState: State<ScreenBehavior>
     fun setScreenBehavior(screenBehavior: ScreenBehavior)
     fun removeScreenBehavior(screenBehavior: ScreenBehavior)
 }
