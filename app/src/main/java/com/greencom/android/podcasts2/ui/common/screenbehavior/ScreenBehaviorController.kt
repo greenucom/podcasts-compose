02b67c378a1a87442ad7b197fe8c4a4e089cfc39
Artem Sidorov <s.artem.ekb@gmail.com>
2022-06-30 22:24:55 +0300

    Add currentScreenBehavior property to ScreenBehaviorController

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorController.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorController.kt
@@ -3,6 +3,7 @@ package com.greencom.android.podcasts2.ui.common.screenbehavior
 import androidx.compose.runtime.staticCompositionLocalOf
 
 interface ScreenBehaviorController {
+    val currentScreenBehavior: ScreenBehavior
     fun setScreenBehavior(screenBehavior: ScreenBehavior)
     fun removeScreenBehavior(screenBehavior: ScreenBehavior)
 }
