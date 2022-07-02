6ad18bea16a68541e5ac92f2d5eaad6a1c998531
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-02 12:50:13 +0300

    Rewrite PodcastRepository.getTrendingPodcasts() to return Flow<Result<List<Podcast>>>

--- app/src/main/java/com/greencom/android/podcasts2/data/podcast/PodcastRepository.kt
+++ app/src/main/java/com/greencom/android/podcasts2/data/podcast/PodcastRepository.kt
@@ -3,7 +3,9 @@ package com.greencom.android.podcasts2.data.podcast
 import com.greencom.android.podcasts2.domain.category.Category
 import com.greencom.android.podcasts2.domain.podcast.Podcast
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import timber.log.Timber
 import javax.inject.Inject
 
 class PodcastRepository @Inject constructor(
@@ -19,14 +21,21 @@ class PodcastRepository @Inject constructor(
         max: Int,
         inCategories: List<Category>,
         notInCategories: List<Category>,
-    ): Flow<List<Podcast>> {
-        val trendingPodcasts = remoteDataSource.getTrendingPodcasts(
-            max = max,
-            inCategories = inCategories,
-            notInCategories = notInCategories,
-        )
-        return localDataSource.userSubscriptionsIds.map { userSubscriptionsIds ->
-            trendingPodcasts.map { it.copy(isUserSubscribed = it.id in userSubscriptionsIds) }
+    ): Flow<Result<List<Podcast>>> {
+        return try {
+            val trendingPodcasts = remoteDataSource.getTrendingPodcasts(
+                max = max,
+                inCategories = inCategories,
+                notInCategories = notInCategories,
+            )
+            localDataSource.userSubscriptionsIds
+                .map { userSubscriptionsIds ->
+                    trendingPodcasts.map { it.copy(isUserSubscribed = it.id in userSubscriptionsIds) }
+                }
+                .map { Result.success(it) }
+        } catch (e: Exception) {
+            Timber.e(e, "Exception occurred while receiving trending podcasts")
+            flow { emit(Result.failure(e)) }
         }
     }
 
--- app/src/main/java/com/greencom/android/podcasts2/domain/podcast/usecase/GetTrendingPodcastsForSelectedTrendingCategoriesUseCase.kt
+++ app/src/main/java/com/greencom/android/podcasts2/domain/podcast/usecase/GetTrendingPodcastsForSelectedTrendingCategoriesUseCase.kt
@@ -7,10 +7,9 @@ import com.greencom.android.podcasts2.di.IODispatcher
 import com.greencom.android.podcasts2.domain.category.Category
 import com.greencom.android.podcasts2.domain.podcast.Podcast
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 class GetTrendingPodcastsForSelectedTrendingCategoriesUseCase @Inject constructor(
@@ -19,24 +18,20 @@ class GetTrendingPodcastsForSelectedTrendingCategoriesUseCase @Inject constructo
     private val categoryRepository: CategoryRepository,
 ) : FlowUseCase<Unit, List<Podcast>>(dispatcher) {
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     override fun execute(params: Unit): Flow<Result<List<Podcast>>> {
         val trendingCategories = categoryRepository.getTrendingCategories()
-
-        return channelFlow {
-            categoryRepository.getSelectedTrendingCategoriesIds()
-                .collectLatest { idsOfSelectedCategories ->
-                    val selectedCategories = trendingCategories
-                        .filter { it.id in idsOfSelectedCategories }
-                    val trendingPodcastsMaxSize = calculateTrendingPodcastsMaxSize(selectedCategories)
-                    podcastRepository.getTrendingPodcasts(
-                        max = trendingPodcastsMaxSize,
-                        inCategories = selectedCategories,
-                        notInCategories = emptyList(),
-                    ).collect {
-                        send(it)
-                    }
-                }
-        }.map { Result.success(it) }
+        return categoryRepository.getSelectedTrendingCategoriesIds()
+            .flatMapLatest { idsOfSelectedCategories ->
+                val selectedCategories = trendingCategories
+                    .filter { it.id in idsOfSelectedCategories }
+                val trendingPodcastsMaxSize = calculateTrendingPodcastsMaxSize(selectedCategories)
+                podcastRepository.getTrendingPodcasts(
+                    max = trendingPodcastsMaxSize,
+                    inCategories = selectedCategories,
+                    notInCategories = emptyList(),
+                )
+            }
     }
 
     private fun calculateTrendingPodcastsMaxSize(
