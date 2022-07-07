c437805c7c86e7231fed1753a0f06764aff55eba
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-07 11:18:31 +0300

    Make SpecificScreenBehavior() inlined

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorEffect.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorEffect.kt
@@ -4,10 +4,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 
 @Composable
-fun SpecificScreenBehavior(builderAction: (ScreenBehaviorBuilder.() -> Unit)) {
+inline fun SpecificScreenBehavior(crossinline builderAction: (ScreenBehaviorBuilder.() -> Unit)) {
     val screenBehaviorController = LocalScreenBehaviorController.current ?: return
     DisposableEffect(Unit) {
-        val screenBehavior = buildScreenBehavior(builderAction)
+        val screenBehaviorBuilder = ScreenBehaviorBuilder()
+        screenBehaviorBuilder.builderAction()
+        val screenBehavior = screenBehaviorBuilder.build()
         screenBehaviorController.setScreenBehavior(screenBehavior)
 
         onDispose { screenBehaviorController.removeScreenBehavior(screenBehavior) }
