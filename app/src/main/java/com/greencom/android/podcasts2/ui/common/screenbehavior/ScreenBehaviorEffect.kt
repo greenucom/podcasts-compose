d9efc2b193cd09165c3180708c4edfcbdb969ac7
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-11 14:03:43 +0300

    Add optional keys to SpecificScreenBehavior

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorEffect.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/screenbehavior/ScreenBehaviorEffect.kt
@@ -2,17 +2,27 @@ package com.greencom.android.podcasts2.ui.common.screenbehavior
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import timber.log.Timber
+
+const val TagScreenBehavior = "ScreenBehavior"
 
 @Composable
-inline fun SpecificScreenBehavior(crossinline builderAction: (ScreenBehaviorBuilder.() -> Unit)) {
+inline fun SpecificScreenBehavior(
+    vararg keys: Any? = arrayOf(Unit),
+    crossinline builderAction: (ScreenBehaviorBuilder.() -> Unit),
+) {
     val screenBehaviorController = LocalScreenBehaviorController.current ?: return
-    DisposableEffect(Unit) {
+    DisposableEffect(*keys) {
         val screenBehaviorBuilder = ScreenBehaviorBuilder()
         screenBehaviorBuilder.builderAction()
         val screenBehavior = screenBehaviorBuilder.build()
         screenBehaviorController.setScreenBehavior(screenBehavior)
+        Timber.tag(TagScreenBehavior).d("SpecificScreenBehavior set")
 
-        onDispose { screenBehaviorController.removeScreenBehavior(screenBehavior) }
+        onDispose {
+            screenBehaviorController.removeScreenBehavior(screenBehavior)
+            Timber.tag(TagScreenBehavior).d("SpecificScreenBehavior removed")
+        }
     }
 }
 
@@ -22,7 +32,11 @@ fun DefaultScreenBehavior() {
     DisposableEffect(Unit) {
         val defaultScreenBehavior = ScreenBehavior.Default
         screenBehaviorController.setScreenBehavior(defaultScreenBehavior)
+        Timber.tag(TagScreenBehavior).d("DefaultScreenBehavior set")
 
-        onDispose { screenBehaviorController.removeScreenBehavior(defaultScreenBehavior) }
+        onDispose {
+            screenBehaviorController.removeScreenBehavior(defaultScreenBehavior)
+            Timber.tag(TagScreenBehavior).d("DefaultScreenBehavior removed")
+        }
     }
 }
