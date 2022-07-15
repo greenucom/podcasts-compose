8cb518668f1c30160c158a8873033c86452cc43e
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-15 20:00:22 +0300

    Add SearchTextField

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/route/search/component/SearchTextField.kt
@@ -0,0 +1,96 @@
+package com.greencom.android.podcasts2.ui.route.search.component
+
+import android.content.res.Configuration
+import androidx.compose.animation.*
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.greencom.android.podcasts2.R
+import com.greencom.android.podcasts2.ui.common.PodcastsIcons
+import com.greencom.android.podcasts2.ui.common.TextFieldCustom
+import com.greencom.android.podcasts2.ui.theme.PodcastsTheme
+import com.greencom.android.podcasts2.ui.theme.searchBackground
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun SearchTextField(
+    value: String,
+    onValueChanged: (String) -> Unit,
+    onClearTextFieldClicked: () -> Unit,
+    onImeSearch: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Surface(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.small,
+        color = MaterialTheme.colors.searchBackground,
+    ) {
+        Row {
+            val textColor = MaterialTheme.colors.onSurface.copy(alpha = 0.87f)
+            TextFieldCustom(
+                value = value,
+                onValueChange = onValueChanged,
+                placeholder = { Text(text = stringResource(R.string.search_for_podcasts)) },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions { onImeSearch() },
+                singleLine = true,
+                shape = MaterialTheme.shapes.small,
+                colors = TextFieldDefaults.textFieldColors(
+                    textColor = textColor,
+                    placeholderColor = textColor,
+                    backgroundColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                ),
+            )
+
+            AnimatedVisibility(
+                modifier = Modifier.height(40.dp),
+                visible = value.isNotEmpty(),
+                enter = scaleIn(spring()) + fadeIn(spring()),
+                exit = scaleOut(spring(stiffness = Spring.StiffnessLow)) +
+                        fadeOut(spring(stiffness = Spring.StiffnessLow)),
+            ) {
+                IconButton(onClick = onClearTextFieldClicked) {
+                    Icon(
+                        imageVector = PodcastsIcons.Close,
+                        contentDescription = stringResource(R.string.content_desc_clear_text_field),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+private fun Preview() {
+    PodcastsTheme {
+        Surface {
+            var value by remember { mutableStateOf("") }
+            SearchTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                value = value,
+                onValueChanged = { value = it },
+                onClearTextFieldClicked = {},
+                onImeSearch = {},
+            )
+        }
+    }
+}
