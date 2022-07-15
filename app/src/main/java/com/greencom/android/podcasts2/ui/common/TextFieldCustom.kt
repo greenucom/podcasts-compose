24d80d6d3ccf0e24cc5ed2525e62fea9a9e52894
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-15 18:29:25 +0300

    Create TextFieldCustom with MinHeight = 40.dp instead of 56.dp

---
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/TextFieldCustom.kt
@@ -0,0 +1,123 @@
+package com.greencom.android.podcasts2.ui.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.shape.ZeroCornerSize
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.TextFieldDefaults.indicatorLine
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+private val MinHeight = 40.dp
+
+private val StartPaddingWithoutLabel = 16.dp
+private val EndPaddingWithoutLabel = 16.dp
+private val TopPaddingWithoutLabel = 8.dp
+private val BottomPaddingWithoutLabel = 8.dp
+
+private val StartPaddingWithLabel = 16.dp
+private val EndPaddingWithLabel = 16.dp
+private val TopPaddingWithLabel = 20.dp
+private val BottomPaddingWithLabel = 10.dp
+
+@Composable
+fun TextFieldCustom(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    readOnly: Boolean = false,
+    textStyle: TextStyle = LocalTextStyle.current,
+    label: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    leadingIcon: @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    isError: Boolean = false,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    singleLine: Boolean = false,
+    maxLines: Int = Int.MAX_VALUE,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    shape: Shape =
+        MaterialTheme.shapes.small.copy(bottomEnd = ZeroCornerSize, bottomStart = ZeroCornerSize),
+    colors: TextFieldColors = TextFieldDefaults.textFieldColors()
+) {
+    // If color is not provided via the text style, use content color as a default
+    val textColor = textStyle.color.takeOrElse {
+        colors.textColor(enabled).value
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
+    @OptIn(ExperimentalMaterialApi::class)
+    BasicTextField(
+        value = value,
+        modifier = modifier
+            .background(colors.backgroundColor(enabled).value, shape)
+            .indicatorLine(enabled, isError, interactionSource, colors)
+            .defaultMinSize(
+                minWidth = TextFieldDefaults.MinWidth,
+                minHeight = MinHeight,
+            ),
+        onValueChange = onValueChange,
+        enabled = enabled,
+        readOnly = readOnly,
+        textStyle = mergedTextStyle,
+        cursorBrush = SolidColor(colors.cursorColor(isError).value),
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        interactionSource = interactionSource,
+        singleLine = singleLine,
+        maxLines = maxLines,
+        decorationBox = @Composable { innerTextField ->
+            // places leading icon, text field with label and placeholder, trailing icon
+            TextFieldDefaults.TextFieldDecorationBox(
+                value = value,
+                visualTransformation = visualTransformation,
+                innerTextField = innerTextField,
+                placeholder = placeholder,
+                label = label,
+                leadingIcon = leadingIcon,
+                trailingIcon = trailingIcon,
+                singleLine = singleLine,
+                enabled = enabled,
+                isError = isError,
+                interactionSource = interactionSource,
+                colors = colors,
+                contentPadding = if (label == null) {
+                    textFieldWithoutLabelPadding()
+                } else {
+                    textFieldWithLabelPadding()
+                },
+            )
+        },
+    )
+}
+
+private fun textFieldWithoutLabelPadding(
+    start: Dp = StartPaddingWithoutLabel,
+    top: Dp = TopPaddingWithoutLabel,
+    end: Dp = EndPaddingWithoutLabel,
+    bottom: Dp = BottomPaddingWithoutLabel,
+): PaddingValues = PaddingValues(start, top, end, bottom)
+
+fun textFieldWithLabelPadding(
+    start: Dp = StartPaddingWithLabel,
+    top: Dp = TopPaddingWithLabel,
+    end: Dp = EndPaddingWithLabel,
+    bottom: Dp = BottomPaddingWithLabel,
+): PaddingValues = PaddingValues(start, top, end, bottom)
