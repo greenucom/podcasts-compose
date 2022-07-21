502b57af088a3bb9042dbb1ee21cc9f1c195bdb9
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-21 23:38:14 +0300

    Refactor Int.toBoolean()

--- app/src/main/java/com/greencom/android/podcasts2/utils/Misc.kt
+++ app/src/main/java/com/greencom/android/podcasts2/utils/Misc.kt
@@ -3,5 +3,5 @@ package com.greencom.android.podcasts2.utils
 fun Int.toBoolean(): Boolean = when (this) {
     0 -> false
     1 -> true
-    else -> throw IllegalArgumentException("Value should be either 0 or 1")
+    else -> error("Value should be either 0 or 1")
 }
