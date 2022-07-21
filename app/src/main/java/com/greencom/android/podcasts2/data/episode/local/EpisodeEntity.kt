18749e8d1e0d2bf391d54c4d6dcb70b86e7001c5
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-21 23:49:11 +0300

    Map GetEpisodesByPodcastIdResponseDto to List<EpisodeEntity>

--- app/src/main/java/com/greencom/android/podcasts2/data/episode/local/EpisodeEntity.kt
+++ app/src/main/java/com/greencom/android/podcasts2/data/episode/local/EpisodeEntity.kt
@@ -3,8 +3,6 @@ package com.greencom.android.podcasts2.data.episode.local
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.greencom.android.podcasts2.utils.Size
-import kotlin.time.Duration
 
 @Entity(tableName = "Episodes")
 data class EpisodeEntity(
@@ -14,12 +12,12 @@ data class EpisodeEntity(
     @ColumnInfo(name = "episode_description") val description: String,
     @ColumnInfo(name = "episode_publication_date") val publicationDate: Long,
     @ColumnInfo(name = "episode_audio_url") val audioUrl: String,
-    @ColumnInfo(name = "episode_audio_size_in_bytes") val audioSize: Size,
-    @ColumnInfo(name = "episode_audio_duration_in_seconds") val audioDuration: Duration,
+    @ColumnInfo(name = "episode_audio_size_in_bytes") val audioSizeInBytes: Long,
+    @ColumnInfo(name = "episode_audio_duration_in_seconds") val audioDurationInSeconds: Int,
     @ColumnInfo(name = "episode_explicit") val explicit: Boolean,
-    @ColumnInfo(name = "episode_season") val season: Int,
-    @ColumnInfo(name = "episode_number") val episodeNumber: Int,
-    @ColumnInfo(name = "episode_type") val episodeType: EpisodeTypeEntity,
+    @ColumnInfo(name = "episode_season") val season: Int?,
+    @ColumnInfo(name = "episode_number") val episodeNumber: Int?,
+    @ColumnInfo(name = "episode_type") val episodeType: EpisodeTypeEntity?,
     @ColumnInfo(name = "episode_image_url") val imageUrl: String,
-    @ColumnInfo(name = "episode_chapters_url") val chaptersUrl: String,
+    @ColumnInfo(name = "episode_chapters_url") val chaptersUrl: String?,
 )
--- app/src/main/java/com/greencom/android/podcasts2/data/episode/remote/dto/GetEpisodesByPodcastIdResponseDto.kt
+++ app/src/main/java/com/greencom/android/podcasts2/data/episode/remote/dto/GetEpisodesByPodcastIdResponseDto.kt
@@ -1,5 +1,7 @@
 package com.greencom.android.podcasts2.data.episode.remote.dto
 
+import com.greencom.android.podcasts2.data.episode.local.EpisodeEntity
+import com.greencom.android.podcasts2.utils.toBoolean
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -8,6 +10,10 @@ data class GetEpisodesByPodcastIdResponseDto(
     @SerialName("items") val episodes: List<EpisodeDto>? = null,
     @SerialName("count") val episodeCount: Int? = null,
 ) {
+
+    fun toEpisodeEntityList(): List<EpisodeEntity> {
+        return episodes?.map { it.toEpisodeEntity() } ?: emptyList()
+    }
 
     @Serializable
     data class EpisodeDto(
@@ -24,6 +30,24 @@ data class GetEpisodesByPodcastIdResponseDto(
         @SerialName("episodeType") val episodeType: String? = null,
         @SerialName("image") val imageUrl: String? = null,
         @SerialName("chaptersUrl") val chaptersUrl: String? = null,
-    )
+    ) {
+
+        fun toEpisodeEntity(): EpisodeEntity = EpisodeEntity(
+            id = checkNotNull(id),
+            title = checkNotNull(title),
+            description = checkNotNull(description),
+            publicationDate = checkNotNull(publicationDate),
+            audioUrl = checkNotNull(audioUrl),
+            audioSizeInBytes = checkNotNull(audioSizeInBytes),
+            audioDurationInSeconds = checkNotNull(audioDurationInSeconds),
+            explicit = explicit?.toBoolean() ?: false,
+            season = season,
+            episodeNumber = episodeNumber,
+            episodeType = episodeType?.let { EpisodeTypeDto.fromString(it).toEpisodeTypeEntity() },
+            imageUrl = checkNotNull(imageUrl),
+            chaptersUrl = chaptersUrl,
+        )
+
+    }
 
 }
