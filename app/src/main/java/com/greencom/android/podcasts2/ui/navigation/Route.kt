7fa44d965558686549ba742b09681e9a577e8a9d
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-23 10:57:40 +0300

    Add Route.Podcast

--- app/src/main/java/com/greencom/android/podcasts2/ui/navigation/Route.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/navigation/Route.kt
@@ -1,12 +1,18 @@
 package com.greencom.android.podcasts2.ui.navigation
 
 import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavDeepLink
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 
 sealed class Route<P>(val routeSchema: String) {
 
     abstract fun createRoute(args: P): String
 
     open val arguments: List<NamedNavArgument>
+        get() = emptyList()
+
+    open val deepLinks: List<NavDeepLink>
         get() = emptyList()
 
     object Podcasts : Route<Unit>(routeSchema = "podcasts") {
@@ -23,6 +29,18 @@ sealed class Route<P>(val routeSchema: String) {
 
     object Search : Route<Unit>(routeSchema = "search") {
         override fun createRoute(args: Unit): String = routeSchema
+    }
+
+    object Podcast : Route<Podcast.Args>(routeSchema = "podcast/{podcastId}") {
+        override fun createRoute(args: Args): String {
+            val (podcastId) = args
+            return "podcast/$podcastId"
+        }
+
+        override val arguments: List<NamedNavArgument>
+            get() = listOf(navArgument("podcastId") { type = NavType.LongType })
+
+        data class Args(val podcastId: Long)
     }
 
 }
