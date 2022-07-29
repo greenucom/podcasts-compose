1ad345189c7e16974871a87239e18c796d240311
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-29 22:09:37 +0300

    Migrate NothingFoundError to use GenericError

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/component/NothingFoundError.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/component/NothingFoundError.kt
@@ -1,52 +1,26 @@
 package com.greencom.android.podcasts2.ui.common.component
 
 import android.content.res.Configuration
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.greencom.android.podcasts2.R
 import com.greencom.android.podcasts2.ui.theme.PodcastsTheme
 
-private val MaxImageSize = 240.dp
-private const val TextAlpha = 0.87f
-
 @Composable
 fun NothingFoundError(modifier: Modifier = Modifier) {
-    Column(
+    GenericError(
         modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Image(
-            modifier = Modifier
-                .widthIn(max = MaxImageSize)
-                .aspectRatio(1f),
-            painter = painterResource(id = R.drawable.vec_image_nothing_found_error),
-            contentDescription = stringResource(id = R.string.nothing_found),
-        )
-
-        Text(
-            modifier = Modifier.alpha(TextAlpha),
-            text = stringResource(id = R.string.nothing_found),
-            style = MaterialTheme.typography.body1,
-            textAlign = TextAlign.Center,
-        )
-    }
+        image = painterResource(id = R.drawable.vec_image_nothing_found_error),
+        description = stringResource(id = R.string.nothing_found),
+    )
 }
 
-@Preview
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Preview(showSystemUi = true)
+@Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 private fun Preview() {
     PodcastsTheme {
