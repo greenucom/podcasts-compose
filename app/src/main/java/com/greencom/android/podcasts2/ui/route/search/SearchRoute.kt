f3203793889367c7c2216c127364e09d18da9682
Artem Sidorov <s.artem.ekb@gmail.com>
2022-07-29 22:38:36 +0300

    Refactor SearchRoute

--- app/src/main/java/com/greencom/android/podcasts2/ui/route/search/SearchRoute.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/route/search/SearchRoute.kt
@@ -1,30 +1,30 @@
 package com.greencom.android.podcasts2.ui.route.search
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material.*
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.greencom.android.podcasts2.R
 import com.greencom.android.podcasts2.ui.common.CrossfadeTyped
 import com.greencom.android.podcasts2.ui.common.animatePlaceholderLoadingColor
 import com.greencom.android.podcasts2.ui.common.component.ConnectionError
+import com.greencom.android.podcasts2.ui.common.component.GenericError
 import com.greencom.android.podcasts2.ui.common.component.NothingFoundError
 import com.greencom.android.podcasts2.ui.common.component.podcast.PodcastItem
 import com.greencom.android.podcasts2.ui.common.component.podcast.PodcastItemPlaceholder
@@ -80,11 +80,13 @@ fun SearchRoute(
         ) { searchResultsState ->
             when (searchResultsState) {
                 SearchViewModel.SearchResultsState.QueryIsEmpty -> {
-                    QueryIsEmpty(
+                    GenericError(
                         modifier = Modifier
                             .fillMaxSize()
                             .padding(horizontal = 16.dp)
-                            .windowInsetsPadding(WindowInsets.ime)
+                            .windowInsetsPadding(WindowInsets.ime),
+                        description = stringResource(id = R.string.enter_search_query),
+                        image = painterResource(id = R.drawable.vec_image_search),
                     )
                 }
 
@@ -165,30 +167,6 @@ fun SearchTopBar(
                 dispatchEvent(event)
             },
             focusRequester = focusRequester,
-        )
-    }
-}
-
-@Composable
-private fun QueryIsEmpty(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Image(
-            modifier = Modifier
-                .widthIn(max = 240.dp)
-                .aspectRatio(1f),
-            painter = painterResource(id = R.drawable.vec_image_search),
-            contentDescription = stringResource(id = R.string.enter_search_query),
-        )
-
-        Text(
-            modifier = Modifier.alpha(0.87f),
-            text = stringResource(id = R.string.enter_search_query),
-            style = MaterialTheme.typography.body1,
-            textAlign = TextAlign.Center,
         )
     }
 }
