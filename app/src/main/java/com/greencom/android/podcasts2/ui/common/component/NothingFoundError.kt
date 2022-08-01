932e22a05f834a2a94724cf7299995c9bd0bcf30
Artem Sidorov <s.artem.ekb@gmail.com>
2022-08-01 14:46:11 +0300

    Add paddings to NothingFoundError

--- app/src/main/java/com/greencom/android/podcasts2/ui/common/component/NothingFoundError.kt
+++ app/src/main/java/com/greencom/android/podcasts2/ui/common/component/NothingFoundError.kt
@@ -1,19 +1,21 @@
 package com.greencom.android.podcasts2.ui.common.component
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.greencom.android.podcasts2.R
 import com.greencom.android.podcasts2.ui.theme.PodcastsTheme
 
 @Composable
 fun NothingFoundError(modifier: Modifier = Modifier) {
     GenericError(
-        modifier = modifier,
+        modifier = modifier.padding(bottom = 24.dp),
         image = painterResource(id = R.drawable.vec_image_nothing_found_error),
         description = stringResource(id = R.string.nothing_found),
     )
